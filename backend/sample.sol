3892b00adafb342ed515393d1189e2c9b68bce2b
kaweendras <it16166134@my.sliit.lk>
2022-03-24 17:52:24 +0530

    fixing error

--- backend/sample.sol
+++ backend/sample.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ArrayOfFacts {
+contract sample {
     string[] private facts;
     address private contractOwner;
 
--- backend/server.js
+++ backend/server.js
@@ -9,31 +9,30 @@ const app = express();
 dotenv.config();
 app.use(cors());
 
-let compiledContract;
+let compiledContract, ABI, BYTECODE;
 
 function compileContract(fileName, contractName) {
   const contractCode = fs.readFileSync(fileName).toString();
 
   let standardCompilerInput = {
-    language: "Solidity",
-    sources: {
-      contract: {
-        content: contractCode,
+      language: "Solidity",
+      sources: {
+          contract: {
+              content: contractCode
+          }
       },
-    },
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["abi", "evm.bytecode"],
-        },
-      },
-    },
+      settings: {
+          outputSelection: {
+              "*": {
+                  "*": ["abi", "evm.bytecode"]
+              }
+          }
+      }
   };
 
   standardCompilerInput = JSON.stringify(standardCompilerInput);
   const output = solc.compile(standardCompilerInput);
   compiledContract = JSON.parse(output).contracts.contract[contractName];
-  console.log(compileContract.abi);
   return compiledContract;
 }
 
@@ -41,12 +40,21 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 8070;
 app.route("/").post(async (req, res) => {
   const { name } = req.body;
+  
+  try{
+    await compileContract("./sample.sol", "sample")
+    ABI = compiledContract.abi;
+    BYTECODE = "0x" + compiledContract.evm.bytecode.object;
 
-  console.log(compileContract("./sample.sol", "sample"));
+    res.send({
+      ABI: ABI,
+      BYTECODE: BYTECODE
+    });
 
-  res.send({
-    data: compiledContract,
-  });
+  }catch(err){
+    console.log(err);
+  }
+  
 });
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
